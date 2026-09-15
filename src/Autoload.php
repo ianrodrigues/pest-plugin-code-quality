c738bf10c12003fc8017e51c37c1923341fddac3
Ian Rodrigues <ian@rdgs.net>
2026-09-15 01:26:14 +0100

    feat: add the toHaveInheritanceDepthAtMost expectation

--- src/Autoload.php
+++ src/Autoload.php
@@ -41,6 +41,11 @@ $register = static function (): void {
         /** @var Expectation<array<int, string>|string> $this */
         return PolicyExpectation::make($this, Policy::properties($max, $allowEmpty));
     });
+
+    expect()->extend('toHaveInheritanceDepthAtMost', function (int $max, bool $allowEmpty = false): ArchExpectation {
+        /** @var Expectation<array<int, string>|string> $this */
+        return PolicyExpectation::make($this, Policy::inheritance($max, $allowEmpty));
+    });
 };
 
 if (function_exists('expect')) {
